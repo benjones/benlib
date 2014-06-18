ac09283fe5f9c3e44f95863fddcf7ba351ec04d0
Ben Jones <ben.james.jones@gmail.com>
2014-06-18 16:20:28 -0600

    prealloc vector with most features implemented

---
+++ tests/preallocVectorTest.cpp
@@ -0,0 +1,83 @@
+#include "../types/preallocVector.hpp"
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+
+template<typename T>
+void dump(const T& t){
+  std::cout << "container dump: " << std::endl;
+  for(auto&& u : t){
+	std::cout << u << std::endl;
+  }
+}
+
+int main(){
+
+  benlib::PreallocVector<double,3> pvec;
+  
+  pvec.push_back(1);
+  pvec.push_back(2);
+  pvec.push_back(3);
+  pvec.push_back(4);
+
+  pvec.push_back(20);  
+  pvec.push_back(15);
+  pvec.push_back(-20);
+  pvec.push_back(0);
+  pvec.push_back(13);
+  pvec.emplace_back(5);
+  
+
+  for(auto d : pvec){
+	std::cout << d << std::endl;
+  }
+  //  const benlib::PreallocVector<double> pvec2;
+  //  auto beg2 = pvec2.begin();
+  std::sort(pvec.begin(), pvec.end());
+  for(auto d : pvec){
+	std::cout << d << std::endl;
+  }
+
+  
+  //assign tests
+  {
+	std::cout << "assign test 1: " << std:: endl;
+	std::vector<int> sv{0,1,2,3,4,5};
+	
+	benlib::PreallocVector<int> pv;
+	pv.assign(sv.begin(), sv.end());
+	dump(pv);
+	assert(pv.size() == sv.size());
+  }
+
+  {
+	std::cout << "assign test 2: " << std:: endl;
+	std::vector<int> sv{0,1,2,3,4,5};
+	
+	benlib::PreallocVector<int, 3> pv;
+	pv.assign(sv.begin(), sv.end());
+	dump(pv);
+	assert(pv.size() == sv.size());
+  }
+
+  {
+	std::cout << "assign test 3: " << std:: endl;
+	std::vector<int> sv{0,1,2,3,4,5};
+	
+	benlib::PreallocVector<int, 3> pv;
+	pv.push_back(3);
+	pv.assign(sv.begin(), sv.end());
+	dump(pv);
+	assert(pv.size() == sv.size());
+  }
+
+  {
+	std::cout << "assign test 4: " << std:: endl;
+	benlib::PreallocVector<int, 3> pv;
+	pv.push_back(3);
+	pv.assign(10ul, 4);
+	dump(pv);
+	assert(pv.size() == 10);
+  }
+
+}
---
+++ types/preallocVector.hpp
@@ -0,0 +1,366 @@
+#pragma once
+
+/*
+  This should be an almost drop in replacement for std::vector,
+  with a few caveats.
+  It allocates the first part of the space statically,
+  and the rest in a std::vector, so if you need a bunch of these, and most
+  will have bounded size, this should improve cache coherence substantially
+  
+
+*/
+#include <iterator>
+#include <type_traits>
+#include <vector>
+#include <cassert>
+
+namespace benlib{
+
+  template<typename T, size_t StaticSize = 8>
+  class PreallocVector{
+
+  public:
+	
+	template<typename VType, typename ContainerType>
+	class PreallocVectorIterator : 
+	  public std::iterator<std::random_access_iterator_tag,
+						   VType>{
+	
+	public:
+	  ~PreallocVectorIterator() = default;
+	  PreallocVectorIterator() = default; //don't do anything
+
+	  //copy constructor and assignment
+	  template <typename otherVType, typename otherContainerType>
+	  PreallocVectorIterator(const PreallocVectorIterator<otherVType, 
+							 otherContainerType>& other)
+		:index{other.index}, container{other.container}
+	  {}
+	  
+	  template<typename otherVType, typename otherContainerType>
+	  PreallocVectorIterator& operator=(const PreallocVectorIterator& other) noexcept{
+		index = other.index;
+		container = other.container;
+		return *this;
+	  }
+
+	  //(in)equality comparison
+	  template <typename otherVType, typename otherContainerType>
+	  friend inline bool 
+	  operator==(const PreallocVectorIterator<VType, ContainerType>& lhs,
+				 const PreallocVectorIterator<otherVType, otherContainerType>& rhs) noexcept{
+		
+		return lhs.index == rhs.index;
+	  }
+	  template <typename otherVType, typename otherContainerType>
+	  friend inline bool 
+	  operator!=(const PreallocVectorIterator<VType, ContainerType>& lhs,
+				 const PreallocVectorIterator<otherVType, otherContainerType>& rhs) noexcept{
+		return !(lhs == rhs);
+	  }
+	  
+	  template <typename otherVType, typename otherContainerType>
+	  friend inline bool 
+	  operator <(const PreallocVectorIterator<VType, ContainerType>& lhs,
+				 const PreallocVectorIterator<otherVType, otherContainerType>& rhs) noexcept{
+		
+		return lhs.index < rhs.index;
+	  }
+
+	  template <typename otherVType, typename otherContainerType>
+	  friend inline bool 
+	  operator <=(const PreallocVectorIterator<VType, ContainerType>& lhs,
+				  const PreallocVectorIterator<otherVType, otherContainerType>& rhs) noexcept{
+		return lhs.index <= rhs.index;
+	  }
+	  
+	  template <typename otherVType, typename otherContainerType>
+	  friend inline bool 
+	  operator >(const PreallocVectorIterator<VType, ContainerType>& lhs,
+				 const PreallocVectorIterator<otherVType, otherContainerType>& rhs) noexcept{
+		return lhs.index > rhs.index;
+	  }
+	  
+	  template <typename otherVType, typename otherContainerType>
+	  friend inline bool 
+	  operator >=(const PreallocVectorIterator<VType, ContainerType>& lhs,
+				  const PreallocVectorIterator<otherVType, otherContainerType>& rhs) noexcept{
+		return lhs.index > rhs.index;
+	  }
+
+
+
+	  //increment
+	  template <typename intType>
+	  PreallocVectorIterator& operator +=(intType amt) noexcept{
+		index += amt;
+		return *this;
+	  }
+	  PreallocVectorIterator& operator ++() noexcept{
+		index++;
+		return *this;
+	  }
+	  PreallocVectorIterator operator ++(int) noexcept{
+		PreallocVectorIterator tmp{*this};
+		operator++();
+		return tmp;
+	  }
+	  
+	  //decrement
+	  template <typename intType>
+	  PreallocVectorIterator& operator -=(intType amt) noexcept{
+		index -= amt;
+		return *this;
+	  }
+	  PreallocVectorIterator& operator --() noexcept{
+		index--;
+		return *this;
+	  }
+	  PreallocVectorIterator operator --(int) noexcept{
+		PreallocVectorIterator tmp{*this};
+		operator--();
+		return tmp;
+	  }
+	  
+	  //arithmetic
+	  template<typename intType>
+	  inline friend PreallocVectorIterator 
+	  operator +(PreallocVectorIterator it, intType n){
+		it += n;
+		return it;
+	  }
+	  template<typename intType>
+	  inline friend PreallocVectorIterator 
+	  operator +(intType n, PreallocVectorIterator it){
+		it += n;
+		return it;
+	  }
+	  template<typename intType>
+	  inline friend PreallocVectorIterator
+	  operator -(PreallocVectorIterator it, intType n){
+		it -= n;
+		return it;
+	  }
+
+	  inline size_t
+	  operator -(PreallocVectorIterator rhs){
+		return index - rhs.index;
+	  }
+
+
+	  //dereferencing
+	  const VType& operator*() const{
+		return container->operator[](index);
+	  }
+	  const VType* operator->() const{
+		return &(container->operator[](index));
+	  }
+	  const VType& operator[](size_t n) const{
+		return container->operator[](index + n);
+	  }
+
+	  //non const derefs for nonconst iterator types
+	  template<typename Dummy = VType>
+	  typename std::enable_if<!std::is_const<Dummy>::value,
+							  VType&>::type
+	  operator*(){
+		return container->operator[](index);
+	  }
+	  template<typename Dummy = VType>
+	  typename std::enable_if<!std::is_const<Dummy>::value,
+							  VType*>::type
+	  operator->(){
+		return &(container->operator[](index));
+	  }
+	  template<typename Dummy = VType>
+	  typename std::enable_if<!std::is_const<Dummy>::value,
+							  VType&>::type
+	  operator[](size_t n){
+		return container->operator[](index + n);
+	  }
+	  
+	  
+					 
+	  template <typename U, size_t V>
+	  friend class PreallocVector;
+	private:
+	  size_t index;
+	  ContainerType* container;
+	  PreallocVectorIterator(size_t _index, ContainerType* _container)
+		:index{_index}, container{_container}
+	  {}
+	};
+
+	
+	
+
+	using value_type = T;
+	using reference = value_type&;
+	using const_refernce = const reference;
+
+	
+	using iterator = PreallocVectorIterator<value_type, 
+											PreallocVector<T, StaticSize>>;
+	using const_iterator = 
+	  PreallocVectorIterator<const value_type, 
+							 const PreallocVector<T, StaticSize>>;
+	
+
+
+	//constructors
+	PreallocVector()  //empty vector
+	noexcept(std::is_nothrow_default_constructible<std::vector<T>>::value)
+	  :size_{0},
+	  data_{reinterpret_cast<T*>(buffer_)}
+	{}
+	  
+	// n default constructed objects
+	explicit PreallocVector(size_t n)
+	  :size_{n},
+	  data_{reinterpret_cast<T*>(buffer_)}
+	{
+	  for(size_t i = 0; i < n && i < StaticSize; ++i){
+		new (data_ + i) value_type();
+	  }
+	  if(n > StaticSize){
+		extraData_.resize(n - StaticSize);
+	  }
+	}
+	
+	// n not-default constructed objects
+	PreallocVector(size_t n, const value_type& value)
+	  : size_{n},
+	  data_{reinterpret_cast<T*>(buffer_)}
+	{
+	  for(size_t i = 0; i < n && i < StaticSize; ++i){
+		new (data_ + i) value_type(value);
+	  }
+	  if(n > StaticSize){
+		extraData_.assign(n - StaticSize, value);
+	  }
+	}
+	
+	// iterator range
+	template <typename InIt>
+	PreallocVector(InIt first, InIt last)
+	  :size_{0},
+	  data_{reinterpret_cast<T*>(buffer_)}
+	{
+	  for(auto it = first; it != last; ++it){
+		push_back(*it);
+		++size_;
+	  }
+	}
+
+
+
+	
+
+	inline void push_back(const value_type& val){
+	  if(size_ < StaticSize){
+		new (data_ + size_) value_type(val);
+		++size_;
+	  } else {
+		extraData_.push_back(val);
+		++size_;
+	  }
+	}
+	
+	inline void pop_back(){
+	  assert(!empty());
+	  if(size_ <= StaticSize){
+		(data_ + size_ -1)->~value_type();
+		--size_;
+	  } else {
+		extraData_.pop_back();
+		--size_;
+	  }
+	}
+	template<typename ... Args>
+	inline void emplace_back(Args&&... args){
+	  if(size_ < StaticSize){
+		new(data_ + size_) value_type(std::forward<Args>(args)...);
+		++size_;
+	  } else {
+		extraData_.emplace_back(std::forward<Args>(args)...);
+		++size_;
+	  }
+	}
+
+	inline void resize(size_t n){
+	  if(n > size_){
+		for(size_t i = size_; i < n; ++i){
+		  emplace_back();
+		}
+	  } else {
+		while(size_ > n){
+		  pop_back();
+		}
+	  }
+	}
+	
+	template <typename InIt>
+	inline void assign(InIt first, InIt last){
+	  size_t newSize = 0;
+	  while(newSize < size_ && first != last){
+		operator[](newSize) = *first++;
+		++newSize;
+	  }
+	  while(first != last){
+		push_back(*first++);
+		++newSize;
+	  }
+	  while(newSize < size_){
+		pop_back();
+	  }
+	  size_ = newSize;
+	}
+
+	inline void assign(size_t n, const value_type& val){
+	  for(size_t i = 0; i < n && i < size_; ++i){
+		operator[](i) = val;
+	  }
+	  for(size_t i = size_; i < n; ++i){
+		push_back(val);
+	  }
+	  assert(size() == n);
+	}
+
+
+	inline value_type& operator[](size_t n){
+	  if(n < StaticSize){
+		return data_[n];
+	  }
+	  return extraData_[n - StaticSize];
+	}
+	
+	inline const value_type& operator[](size_t n) const {
+	  if(n < StaticSize){
+		return data_[n];
+	  }
+	  return extraData_[n - StaticSize];
+	}
+	
+	
+	
+	
+	inline size_t size() const {return size_;}
+	inline size_t staticSize() const { return StaticSize;}
+	inline bool empty() const {return size_ == 0;}
+	
+	inline iterator begin(){ return iterator{0, this};}
+	inline const_iterator begin() const {return const_iterator{0, this};}
+	
+	inline iterator end(){ return iterator{size_, this};}
+	inline const_iterator end() const { return const_iterator{size_, this};}
+		  
+  private:
+	size_t size_;
+	char* buffer_[sizeof(T)*StaticSize];
+	T* data_; //always points to buffer
+	std::vector<T> extraData_;
+
+
+  };
+
+}
